1224cc5e0db9a14805f4c7b49d4bbbf4a6e429cb
Shay <moeshakasanova@gmail.com>
2024-04-17 18:00:57 +0400

    playNextSong function

--- script.js
+++ script.js
@@ -105,6 +105,16 @@ const pauseSong = () => {
   playButton.classList.remove("playing");
   audio.pause();
 };
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
+  }
+};
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -126,6 +136,8 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+const getCurrentSongIndex = () =>
+  userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
@@ -135,6 +147,7 @@ playButton.addEventListener("click", () => {
   }
 });
 pauseButton.addEventListener("click", pauseSong);
+nextButton.addEventListener("click", playNextSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
